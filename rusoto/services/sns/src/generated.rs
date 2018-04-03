a9eee67c4b5c2260d629f81601c67df602eb1e59
Kevin King <kevin@krypt.co>
2018-04-02 23:56:46 -0400

    Manually fix MapStringToStringSerializer

--- rusoto/services/sns/src/generated.rs
+++ rusoto/services/sns/src/generated.rs
@@ -1622,7 +1622,7 @@ impl MapStringToStringSerializer {
         obj: &::std::collections::HashMap<String, String>,
     ) {
         for (index, (key, value)) in obj.iter().enumerate() {
-            let prefix = format!("{}.{}", name, index + 1);
+            let prefix = format!("{}.entry.{}", name, index + 1);
             params.put(&format!("{}.{}", prefix, "key"), &key);
             params.put(&format!("{}.{}", prefix, "Value"), &value);
         }
